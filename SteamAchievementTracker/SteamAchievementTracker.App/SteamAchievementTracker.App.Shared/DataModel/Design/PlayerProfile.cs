81dbfc52ce895a9bc002db8169f3bfa1be139acb
Daniel Worthy <danieljworthy@gmail.com>
2014-06-16 06:41:36 -0500

    asdf

---
+++ SteamAchievementTracker/SteamAchievementTracker.App/SteamAchievementTracker.App.Shared/DataModel/Design/PlayerProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamAchievementTracker.App.DataModel {
+    public partial class PlayerProfile {
+        public void PopulateDesignData() {
+            this.Name = "Daniel Worthy";
+            
+
+
+
+        }
+
+    }
+}
