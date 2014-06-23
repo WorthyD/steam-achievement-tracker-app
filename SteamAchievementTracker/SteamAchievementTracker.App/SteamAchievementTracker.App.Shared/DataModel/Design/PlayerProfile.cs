5a1ec7743b18d5b6e4efbf72483bf7edb3a7f5ae
Daniel Worthy <danieljworthy@gmail.com>
2014-06-23 06:25:29 -0500

    kjjkjk

--- SteamAchievementTracker/SteamAchievementTracker.App/SteamAchievementTracker.App.Shared/DataModel/Design/PlayerProfile.cs
+++ SteamAchievementTracker/SteamAchievementTracker.App/SteamAchievementTracker.App.Shared/DataModel/Design/PlayerProfile.cs
@@ -7,9 +7,6 @@ namespace SteamAchievementTracker.App.DataModel {
         public void PopulateDesignData() {
             this.Name = "Daniel Worthy";
             this.ThumbURL = "http://www.gravatar.com/avatar/67b727175a880f13e6240c856764670e.png?s=50";
-            
-
-
 
         }
 
