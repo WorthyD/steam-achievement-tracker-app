8ad74095bef64fdd486885901f9720d47f88208a
Daniel Worthy <danieljworthy@gmail.com>
2014-07-14 05:49:18 -0500

    Adding Data access

---
+++ SteamAchievementTracker/SteamAchievementTracker.App.DataAccess/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamAchievementTracker.App.DataAccess
+{
+    public class Class1
+    {
+    }
+}
