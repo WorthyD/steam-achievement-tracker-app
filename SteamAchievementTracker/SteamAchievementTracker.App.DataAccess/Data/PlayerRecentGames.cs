4b0b55de1a8870c5bb03859639210a31c62575e1
Daniel Worthy <danieljworthy@gmail.com>
2014-09-13 14:29:35 -0500

    adding stuff

---
+++ SteamAchievementTracker/SteamAchievementTracker.App.DataAccess/Data/PlayerRecentGames.cs
@@ -0,0 +1,116 @@
+﻿using SQLitePCL;
+using SteamAchievementTracker.Contracts.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAchievementTracker.App.DataAccess.Data
+{
+    public class PlayerRecentGames : TableModelBase<IRecentGame, string>
+    {
+        public override string CreateTable()
+        {
+            return @"CREATE TABLE IF NOT EXISTS  [PlayerRecentGames] (	[ID64] INTEGER  NOT NULL ,	[GameLink] vARCHAR(150) NOT NULL)";
+        }
+
+        protected override IRecentGame CreateItem(SQLitePCL.ISQLiteStatement statement)
+        {
+            IRecentGame game = null;
+
+            game.GameLink = statement["GameLink"].ToSafeString();
+            game.ID64 = statement["ID64"].ToLong();
+
+            return game;
+        }
+
+
+        protected override string GetInsertItemSql()
+        {
+            return "INSERT INTO PlayerRecentGames (ID64, GameLink) VALUES  ( @ID64, @GameLink );";
+        }
+
+        protected override void FillInsertStatement(SQLitePCL.ISQLiteStatement statement, IRecentGame item)
+        {
+            statement.Bind("@id64", item.ID64);
+            statement.Bind("@GameLink", item.GameLink);
+        }
+
+        protected override IRecentGame GetEmpty()
+        {
+            return null;
+        }
+
+        protected override void FillDeleteItemStatement(SQLitePCL.ISQLiteStatement statement, string key)
+        {
+            statement.Bind("@id64", key);
+        }
+
+        protected override string GetDeleteItemSql()
+        {
+            return "DELETE FROM PlayerRecentGames WHERE ID64 = pid64 ;";
+        }
+
+        public List<IRecentGame> GetByUserID(long key)
+        {
+            var items = new ObservableCollection<IRecentGame>();
+            string sqlStatement = "SELECT ID64, GameLink FROM PlayerRecentGames	WHERE ID65 = @ID64;";
+            using (var statement = base.sqlConnection.Prepare(sqlStatement))
+            {
+                statement.Bind("@ID64", key);
+                while (statement.Step() == SQLiteResult.ROW)
+                {
+                    var item = CreateItem(statement);
+                    items.Add(item);
+                }
+            }
+            Timestamp = DateTime.Now;
+            return items.ToList();
+        }
+
+
+
+        /// <summary>
+        /// /////////////////////////////////Not implemented
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        protected override string GetUpdateItemSql()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override string GetSelectItemSql()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void FillSelectItemStatement(SQLitePCL.ISQLiteStatement statement, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override string GetSelectAllSql()
+        {
+            throw new NotImplementedException();
+        }
+        protected override void FillUpdateStatement(SQLitePCL.ISQLiteStatement statement, string key, IRecentGame item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        protected override void FillSelectAllStatement(SQLitePCL.ISQLiteStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
