f58041c755d63bba4b462885b8bf42210aaaa4d2
Daniel Worthy <danieljworthy@gmail.com>
2014-09-13 14:29:03 -0500

    changes

---
+++ SteamAchievementTracker/SteamAchievementTracker.Contracts/Model/IRecentGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAchievementTracker.Contracts.Model
+{
+    public interface IRecentGame
+    {
+        long ID64 { get; set; }
+        string GameLink { get; set; }
+    }
+}
