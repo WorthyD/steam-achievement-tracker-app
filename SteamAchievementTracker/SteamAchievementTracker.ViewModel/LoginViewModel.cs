1e7abf0f197b4a6b79953d9efdb8c896ed0b9921
Daniel Worthy <danieljworthy@gmail.com>
2014-12-01 06:30:14 -0600

    Stuff

--- SteamAchievementTracker/SteamAchievementTracker.ViewModel/LoginViewModel.cs
+++ SteamAchievementTracker/SteamAchievementTracker.ViewModel/LoginViewModel.cs
@@ -14,7 +14,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Google.Apis.Analytics;
+//using Google.Apis.Analytics;
 
 namespace SteamAchievementTracker.ViewModel
 {
