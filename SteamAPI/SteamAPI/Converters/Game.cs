ee703ca2e6904fe3b8b2c39daf139e2eb2dd3edb
Daniel Worthy <danieljworthy@gmail.com>
2015-01-06 18:56:06 -0600

    STuff

---
+++ SteamAPI/SteamAPI/Converters/Game.cs
@@ -0,0 +1,44 @@
+﻿using SteamAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using SteamAPI.Extensions;
+
+namespace SteamAPI.Converters {
+    public static class Game {
+        public static gamesList StringToGameList(this string gl) {
+
+            gamesList retVal = new gamesList();
+
+            List<gamesListGame> gameList = new List<gamesListGame>();
+
+            XDocument doc = XDocument.Parse(gl);
+            var r = doc.Root;
+            var mrg = r.Element("games");
+            if (mrg != null) {
+                var gs = mrg.Descendants("game");
+                foreach (var x in gs) {
+
+                    gameList.Add(x.ConvertToGame());
+                }
+            }
+
+            return null;
+        }
+        public  static gamesListGame ConvertToGame(this XElement x) {
+            gamesListGame g = new gamesListGame();
+            g.appID = x.GetInt32Element("appID", 0);
+            g.name = x.GetStringValue("name");
+            g.logo = x.GetStringValue("logo");
+            g.storeLink = x.GetStringValue("storeLink");
+            g.hoursLast2Weeks = x.GetDecimalAttribute("hoursLast2Weeks", 0);
+            g.hoursOnRecord = x.GetDecimalAttribute("hoursOnRecord", 0);
+            g.statsLink = x.GetStringValue("statsLink");
+
+            return g;
+        }
+    }
+}
---
+++ SteamAPI/SteamAPI/Converters/Profile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using SteamAPI.Extensions;
+
+namespace SteamAPI.Converters {
+    public static class Profile {
+        public static Models.Profile.profile StringToProfile(this string pro) {
+            var m = new Models.Profile.profile();
+
+            XDocument doc = XDocument.Parse(pro);
+            var r = doc.Root;
+            //m.avatarFull = r.Element("avatarFull").Value;
+            m.steamID64 = r.GetInt64Element("steamID64", 0);
+            m.steamID = r.GetStringValue("steamID");
+
+            m.avatarIcon = r.GetStringValue("avatarIcon");
+            m.avatarMedium = r.GetStringValue("avatarMedium");
+            m.avatarFull = r.GetStringValue("avatarFull");
+
+
+            m.customURL = r.GetStringValue("customURL");
+            m.realname = r.GetStringValue("realname");
+
+            //Get most recent games
+
+            var gameList = new List<Models.Profile.profileMostPlayedGame>();
+            var mrg = r.Element("mostPlayedGames");
+
+            if (mrg != null) {
+                var gs = mrg.Descendants("mostPlayedGame");
+                if (gs != null) {
+                    foreach (var x in gs) {
+                        Models.Profile.profileMostPlayedGame g = new Models.Profile.profileMostPlayedGame();
+                        g.gameLink = x.GetStringValue("gameLink");
+                        gameList.Add(g);
+                    }
+                }
+
+            }
+
+            m.mostPlayedGames = gameList.ToArray();
+
+            return m;
+        }
+    }
+}
