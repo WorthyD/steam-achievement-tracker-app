dced2c87cd12244ea37406368ff8470daf788b0b
Daniel Worthy <danieljworthy@gmail.com>
2015-03-11 05:39:36 -0500

    Stuff

---
+++ WebApp/SteamAchievementTracker.WebApp/SteamAPI.Net.Tests/Player.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAPI.Net.Tests {
+    [TestClass]
+    public class Player {
+        [TestMethod]
+        public async Task W8GetPlayerProfileByName() {
+            bool isTrue = await SteamAPI.Tests.Agnostic.Profile.GetPlayerProfileByName();
+            Assert.IsTrue(isTrue);
+        }
+        [TestMethod]
+        public async Task W8GetPlayerProfileByID() {
+            bool isTrue = await SteamAPI.Tests.Agnostic.Profile.GetPlayerProfileByID();
+            Assert.IsTrue(isTrue);
+        }
+    }
+}
