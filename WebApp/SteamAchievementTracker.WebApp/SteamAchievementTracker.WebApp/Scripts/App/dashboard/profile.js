fc35c8cc0f4691ffedf964144d6dd39daa429e78
Daniel Worthy <danieljworthy@gmail.com>
2015-03-17 22:30:11 -0500

    STuff

---
+++ WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.WebApp/Scripts/App/dashboard/profile.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.dashboard')
+        .controller('Profile', Profile);
+
+    Profile.$inject = ['$q', 'dataservice', 'logger', '$routeParams'];
+
+    function Profile($q, dataservice, logger, $routeParams) {
+        /*jshint validthis: true */
+        var vm = this;
+
+        //vm.avengerCount = 0;
+        vm.profile = null;
+        vm.title = 'Profile';
+
+        activate();
+
+        function activate() {
+            var promises = [getAvengersCast()];
+            return $q.all(promises).then(function () {
+                logger.info('Activated profile View');
+            });
+        }
+
+        function getAvengersCast() {
+            return dataservice.getUserById($routeParams.profileid).then(function (data) {
+                vm.profile = data;
+                return vm.profile;
+            });
+        }
+    }
+})();
