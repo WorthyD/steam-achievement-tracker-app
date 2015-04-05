fcd113d065a685125bd4b2a0dd937120c349aee6
Daniel Worthy <danieljworthy@gmail.com>
2015-04-04 20:57:38 -0500

    STuff

--- WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.WebApp/Scripts/App/Core/datastore.js
+++ WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.WebApp/Scripts/App/Core/datastore.js
@@ -1,12 +1,19 @@
 ﻿(function () {
     'use strict';
-    angular
-        .module('app.core')
-        .factory('datastore', datastore)
+    //angular
+      //  .module('app.core', ['indexedDB']).config(datastore);
+        //.factory('datastore', datastore)
 
     /* @ngInject */
     function datastore() {
-
+        console.log('buidling datastore')
+        $indexedDBProvider
+      .connection('myIndexedDB')
+      .upgradeDatabase(1, function (event, db, tx) {
+          var objStore = db.createObjectStore('people', { keyPath: 'ssn' });
+          objStore.createIndex('name_idx', 'name', { unique: false });
+          objStore.createIndex('age_idx', 'age', { unique: false });
+      });
     };
 
 
