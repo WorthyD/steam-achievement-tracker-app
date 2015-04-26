3deff1618be6afc942438b5a992a661838890d98
Daniel Worthy <danieljworthy@gmail.com>
2015-04-25 22:00:17 -0500

    STuff

--- WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.DataAccess/Models/PlayerGame.cs
+++ WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.DataAccess/Models/PlayerGame.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,31 +9,64 @@ using System.Threading.Tasks;
 namespace SteamAchievementTracker.DataAccess.Models {
     public class PlayerGame {
 
+
+        [Key, Column(Order = 10)]
         public long PlayerID64 { get; set; }
+
+        [Key, Column(Order = 20)]
         public int AppID { get; set; }
 
 
-
+        [Required]
+        [StringLength(250)]
         public string Name { get; set; }
+
+        [Required]
         public string StatsLink { get; set; }
+
+         [Required]
+        [StringLength(250)]
+ 
         public string GameLink { get; set; }
+
+        [Required]
+        [StringLength(250)]
         public string Logo { get; set; }
+
+        [Required]
+        [StringLength(250)]
+ 
         public string Icon { get; set; }
+
+        [Required]
         public decimal HoursPlayed { get; set; }
+
+        [Required]
         public decimal RecentHours { get; set; }
+
+        [Required]
         public DateTime LastUpdated { get; set; }
+
+        [Required]
         public DateTime AchievementRefresh { get; set; }
 
+        [Required]
         public bool RefreshAchievements { get; set; }
+
+        [Required]
         public int AchievementsEarned { get; set; }
-        public int AchievementsLocked { get; }
+
+        [Required]
+        public int AchievementsLocked { get; set; }
+
+        [Required]
         public int TotalAchievements { get; set; }
 
+
+        [Required]
         public bool HasAchievements { get; set; }
 
-
-
-        public bool BeenProcessed { get; }
+        public bool BeenProcessed { get;}
         public double PercentComplete { get; }
 
 
