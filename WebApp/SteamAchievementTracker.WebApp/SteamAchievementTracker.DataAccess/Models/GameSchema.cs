0642b8cbac1a657c7bbd2fc43503c74f138eda52
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-02-02 12:26:21 -0600

    Getting achiecement provider built up

---
+++ WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.DataAccess/Models/GameSchema.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAchievementTracker.DataAccess.Models
+{
+    public class GameSchema
+    {
+        [Required]
+        [Key, Column(Order = 10), DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public long AppId { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime LastAchievementUpdate { get; set; }
+
+        public virtual IList<GameAchievement> GameAchievements { get; set; }
+    }
+}
