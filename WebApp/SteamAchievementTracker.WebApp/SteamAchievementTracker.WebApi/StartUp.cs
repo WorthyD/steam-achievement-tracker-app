488c035ebb29032618c67f685dea0840288c9541
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-07-01 21:57:22 -0500

    Stuff

---
+++ WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.WebApi/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SteamAchievementTracker.WebApi.Startup))]
+namespace SteamAchievementTracker.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
