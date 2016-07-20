120f981773bb9c015c911b6b0c512cccce74a740
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-07-20 04:19:23 -0500

    STuff

--- WebApp/SteamAchievementTracker.WebApp/steam-achievement-tracker/src/app/dashboard/dashboard.component.ts
+++ WebApp/SteamAchievementTracker.WebApp/steam-achievement-tracker/src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
+
+import {IGame} from '../services/models/game';
+import {RecentGamesService} from '../services/recent-games.service';
+
+
 @Component({
   moduleId: module.id,
   selector: 'app-dashboard',
   templateUrl: 'dashboard.component.html',
   styleUrls: ['dashboard.component.css'],
-   directives: [ProfileComponent],
+  directives: [ProfileComponent],
 })
 export class DashboardComponent implements OnInit {
+  recentGames: IGame[];
 
-  constructor() {}
+  constructor(private recentGameService: RecentGamesService) { }
 
   ngOnInit() {
+    this.loadRecentGames();
+  }
+
+  loadRecentGames() {
+
+    this.recentGameService.getRecentGames().subscribe((x: IGame[]) => {
+      this.recentGames = x;
+    });
+
   }
 
 }
