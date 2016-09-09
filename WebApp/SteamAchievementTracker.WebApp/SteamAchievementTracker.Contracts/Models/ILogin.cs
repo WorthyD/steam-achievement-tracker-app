24fe5a1e57b6fa3b717ac47c8d0aaae96ad999d9
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-09-09 14:50:36 -0500

    Stuff

---
+++ WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.Contracts/Models/ILogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAchievementTracker.Contracts.Models
+{
+    public interface ILogin
+    {
+        long SteamId { get; set; }
+        bool Success { get; set; }
+        string Message { get; set; }
+    }
+
+}
---
+++ WebApp/SteamAchievementTracker.WebApp/SteamAchievementTracker.WebApi/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SteamAchievementTracker.WebApi.ViewModels
+{
+    public class LoginViewModel
+    {
+        public int MyProperty { get; set; }
+    }
+}
